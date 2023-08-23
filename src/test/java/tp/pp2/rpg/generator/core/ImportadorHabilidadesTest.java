9d0fb50bba8e322d329bcdd05a0aaac5636c4d9e
Fabián Juárez <juarefa320@gmail.com>
2023-08-23 17:16:34 -0300

    Subida de test para pasar pipeline, luego agregarlo bien

---
+++ src/test/java/tp/pp2/rpg/generator/core/ImportadorHabilidadesTest.java
@@ -0,0 +1,17 @@
+package tp.pp2.rpg.generator.core;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import tp.pp2.rpg.generator.core.importadorHabilidades.ImportadorHabilidades;
+import tp.pp2.rpg.generator.core.importadorHabilidades.ImportadorHabilidadesXLSX;
+
+public class ImportadorHabilidadesTest {
+	@Test
+	public void someMethodReturnsTrue() {
+		ImportadorHabilidades classUnderTest = new ImportadorHabilidadesXLSX();
+		assertTrue("someLibraryMethod should return 'true'", true);
+	}
+
+}
