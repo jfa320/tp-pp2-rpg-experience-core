9f27536168204771f4bc2f077db16563191f26aa
jfa320 <juarefa320@gmail.com>
2023-08-23 21:22:03 -0300

    Subo observable del modelo

--- src/main/java/tp/pp2/rpg/generator/core/entidades/asignadorHabilidades/AsignadorHabilidades.java
+++ src/main/java/tp/pp2/rpg/generator/core/entidades/asignadorHabilidades/AsignadorHabilidades.java
@@ -2,21 +2,38 @@ package tp.pp2.rpg.generator.core.entidades.asignadorHabilidades;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Observable;
 
 import tp.pp2.rpg.generator.core.importadorHabilidades.ImportadorHabilidades;
 import tp.pp2.rpg.generator.core.importadorHabilidades.ImportadorHabilidadesXLSX;
 
-public class AsignadorHabilidades {
-	ImportadorHabilidades importadorHabilidades;
+public class AsignadorHabilidades extends Observable {
+	private ImportadorHabilidades importadorHabilidades;
+	private boolean archivoLeido;
 	
-	public AsignadorHabilidades() {}
+	public AsignadorHabilidades() {
+		archivoLeido=false;
+	}
 	
 	public void asignarHabilidades(File archivoLeer) {
 		importadorHabilidades=new ImportadorHabilidadesXLSX();
 		try {
 			importadorHabilidades.importarHabilidades(archivoLeer);
+			this.setArchivoLeido(true);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
+
+	public boolean isArchivoLeido() {
+		return archivoLeido;
+	}
+
+	public void setArchivoLeido(boolean archivoLeido) {
+		this.archivoLeido = archivoLeido;
+		//notifico a los observers del cambio
+		 setChanged();
+	     notifyObservers(archivoLeido);
+	}
+		
 }
