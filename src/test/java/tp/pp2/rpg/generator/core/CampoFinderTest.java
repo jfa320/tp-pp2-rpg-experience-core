f0b62868c09b85a4ad7dc1dc14916dc3784501b3
Fabián Juárez <juarefa320@gmail.com>
2023-09-11 18:38:08 -0300

    agrego clases con interfaz ajustada

--- src/test/java/tp/pp2/rpg/generator/core/CampoFinderTest.java
+++ src/test/java/tp/pp2/rpg/generator/core/CampoFinderTest.java
@@ -13,16 +13,10 @@ public class CampoFinderTest {
     @Test
 	public void pruebaFind() throws Exception {
     	//System.getProperty para pararme donde esta el proyecto
-        String path =  System.getProperty("user.dir")+"\\src\\test\\java\\tp\\pp2\\rpg\\generator\\core";	
+        String path =  System.getProperty("user.dir")+"\\src\\test\\resources\\plugins";	
         CampoFinder campo = new CampoFinder();
-     // Set<Object> clases=campo.findClassesJAR(path);
         Set<Campo> clases=campo.findClasses(path);
         clases.forEach(c->System.out.println(c.getClass()));
-        for(Package p: Package.getPackages()) {
-        	if(p.getName().startsWith("tp.pp2")) {
-        		System.out.println(p.getName());
-        	}
-        };
 	}
 
 }
