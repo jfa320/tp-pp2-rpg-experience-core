3d22afac29e24cb14265d6a69c593e6f4c56d738
Martin <martinkapo99@gmail.com>
2023-10-08 19:57:36 -0300

    cambios en test y inicializer

--- src/main/java/tp/pp2/rpg/experience/core/entidades/rpg/experience/BatallaInitializer.java
+++ src/main/java/tp/pp2/rpg/experience/core/entidades/rpg/experience/BatallaInitializer.java
@@ -9,21 +9,15 @@ import tp.pp2.rpg.experience.core.entidades.interfaces.Habilidad;
 import tp.pp2.rpg.experience.core.extensible.HabilidadFinder;
 
 public class BatallaInitializer {
-    
-    private Map<String,Properties> personajes;
-    private List<Habilidad> habilidades;
 
-    public Batalla generarBatalla(String pathConfigProperties){
-        try{
-			PersonajeBuilder personajesBuilder = new PersonajeBuilder(pathConfigProperties);
-			personajes = personajesBuilder.buildAllPersonajes();
-			HabilidadFinder habilidadFinder = new HabilidadFinder(pathConfigProperties);
-			habilidades = habilidadFinder.findClasses("");
-		}
-		catch(Exception e){}
+    public Batalla generarBatalla(String pathConfigProperties) throws Exception{
 
-		Batalla batalla = BatallaBuilder.build(habilidades,personajes);
-				
+		PersonajeBuilder personajesBuilder = new PersonajeBuilder(pathConfigProperties);
+		Map<String,Properties> personajes = personajesBuilder.buildAllPersonajes();
+		HabilidadFinder habilidadFinder = new HabilidadFinder(pathConfigProperties);
+		List<Habilidad> habilidades = habilidadFinder.findClasses("");
+
+		Batalla batalla = BatallaBuilder.build(habilidades,personajes);			
         return batalla;
     }
 
