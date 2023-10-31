ebcc3cab3ccb1ae4709cf1bc3fa996caf9b3a1bc
Martin <martinkapo99@gmail.com>
2023-10-31 08:08:11 -0300

    file separator fix

--- src/main/java/tp/pp2/rpg/experience/core/entidades/rpg/experience/BatallaInitializer.java
+++ src/main/java/tp/pp2/rpg/experience/core/entidades/rpg/experience/BatallaInitializer.java
@@ -1,5 +1,6 @@
 package tp.pp2.rpg.experience.core.entidades.rpg.experience;
 
+import java.io.File;
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
@@ -11,10 +12,9 @@ import tp.pp2.rpg.experience.core.extensible.HabilidadFinder;
 public class BatallaInitializer {
 
     public Batalla generarBatalla(String pathConfigProperties){
-
-		PersonajeBuilder personajesBuilder = new PersonajeBuilder(pathConfigProperties);
+		PersonajeBuilder personajesBuilder = new PersonajeBuilder(pathConfigProperties.replace("\\", File.separator));
 		Map<String,Properties> personajes = personajesBuilder.buildAllPersonajes();
-		HabilidadFinder habilidadFinder = new HabilidadFinder(pathConfigProperties);
+		HabilidadFinder habilidadFinder = new HabilidadFinder(pathConfigProperties.replace("\\", File.separator));
 		List<Habilidad> habilidades = null;
 		try {
 			habilidades = habilidadFinder.findClasses("");
