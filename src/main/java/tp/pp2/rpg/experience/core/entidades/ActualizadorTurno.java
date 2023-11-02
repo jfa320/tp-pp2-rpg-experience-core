36f86cb200b435fe7e3bb2b781d72eb84b75b694
Martin <martinkapo99@gmail.com>
2023-11-02 14:15:20 -0300

    arreglo de cambio turno

--- src/main/java/tp/pp2/rpg/experience/core/entidades/ActualizadorTurno.java
+++ src/main/java/tp/pp2/rpg/experience/core/entidades/ActualizadorTurno.java
@@ -1,22 +1,17 @@
 package tp.pp2.rpg.experience.core.entidades;
 
-import java.util.ArrayList;
-import java.util.Properties;
-
 import tp.pp2.rpg.experience.core.entidades.estados.EstadoBatalla;
 
 public class ActualizadorTurno {
-	private ArrayList<Properties> personajes;
 
 	public ActualizadorTurno(Batalla batalla) {
-		this.personajes = batalla.getPersonajes();
 		batalla.setPersonajeActual(1);
 	}
 
 	public void cambiarTurno(Batalla batalla) {
 		if (!batalla.getEstado().equals(EstadoBatalla.FINALIZADA)) {
 			int index = batalla.getPersonajeActual();
-			if (index < personajes.size() - 1)
+			if (index < batalla.getPersonajes().size())
 				batalla.setPersonajeActual(index + 1);
 			else
 				batalla.setPersonajeActual(1);
